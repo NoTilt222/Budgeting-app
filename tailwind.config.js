714efeec01a07fbb268493d73467168a2d1a2e63
Josh <josh.k@tad.sr>
2022-11-13 11:03:40 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./+(blocks|components|layouts|pages|ui)/**/*.+(html|js|jsx|ts|tsx)",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
