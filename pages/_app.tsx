a09e0c124e9ebb441a6345327931d9137a4ebd84
Josh <josh.k@tad.sr>
2022-11-15 05:45:55 -0300

    fix: next head breaking mantine styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
+import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Head>
-      <title>BudgetBear</title>
-      <link rel="shortcut icon" href="/BudgetBear.png" />
-      <Component {...pageProps} />
-    </Head>
+    <>
+      <Head>
+        <title>BudgetBear</title>
+        <link rel="shortcut icon" href="/BudgetBear.png" />
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
 }
