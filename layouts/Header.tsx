7669f3c19ca787223dde64baab74c692bf22a483
Josh <josh.k@tad.sr>
2022-11-15 15:28:04 -0300

    refactor: global layout

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -1,25 +1,29 @@
 import { Icon } from "@iconify/react";
 import { Burger, Drawer, Image } from "@mantine/core";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Header() {
   const [opened, setOpened] = useState(false);
   return (
     <>
-      <section className="bg-primary-600 flex px-6 justify-between items-center pt-8 rounded-b-lg">
-        <Icon
-          icon="fluent:chevron-left-24-regular"
-          height={24}
-          className="text-white"
-        />
-        <div className="w-20">
-          <Image src="/BudgetBear.png" alt="logo" />
+      <section className="bg-primary-600 flex px-6 justify-between items-center py-4 rounded-b-lg">
+        <Link href="/">
+          <div className="w-14">
+            <Image src="/BudgetBear.png" alt="logo" />
+          </div>
+        </Link>
+
+        <div className="flex gap-4 items-center">
+          <Link href="./profile">
+            <Icon icon="bi:person-fill" className="text-white" height={30} />
+          </Link>
+          <Burger
+            opened={opened}
+            onClick={() => setOpened((o) => !o)}
+            color="#ffff"
+          />
         </div>
-        <Burger
-          opened={opened}
-          onClick={() => setOpened((o) => !o)}
-          color="#ffff"
-        />
       </section>
       <Drawer
         opened={opened}
