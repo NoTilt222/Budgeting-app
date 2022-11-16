10930191338fa34e6fb0365652d5e778d05a5f35
NoTilt222 <108106585+NoTilt222@users.noreply.github.com>
2022-11-16 17:52:42 -0300

    feat(notification): add notification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/globals.css";
+import { NotificationsProvider } from '@mantine/notifications';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/BudgetBear.png" />
       </Head>
       <MantineProvider withGlobalStyles withNormalizeCSS>
+      <NotificationsProvider position="top-right" autoClose={800}>
         <Component {...pageProps} />
+      </NotificationsProvider>
       </MantineProvider>
     </>
   );
